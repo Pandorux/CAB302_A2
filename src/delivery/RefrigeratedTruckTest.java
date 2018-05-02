191839a3d5851bdd3b8a11771432f6093368dcb9
Jared Carey <itspandorux@gmail.com>
2018-05-02 21:13:08 +1000

    Added JUnit5 to Project, Created Testcases for each Class

---
+++ src/delivery/RefrigeratedTruckTest.java
@@ -0,0 +1,14 @@
+package delivery;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RefrigeratedTruckTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/store/StockTest.java
@@ -0,0 +1,14 @@
+package store;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StockTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
