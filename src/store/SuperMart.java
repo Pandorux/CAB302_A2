270ad670af071b7d77ad6c4f48a8495453e9edab
QUTAD\N9999884 <reaganmarshall3@gmail.com>
2018-05-11 13:01:34 +1000

    Merge 1

---
+++ src/store/SuperMart.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.util.ArrayList;
+
+public class SuperMart {
+
+	//public static final String Product_reorder_amount = null;
+
+
+
+	//public static final String Product_name = null;
+
+
+
+		static double capital = 0;//intially the mart has no value 
+	
+		Store Store = new Store();
+	
+		static String Product_name;
+		static Integer Product_Quantity;
+		static Double Product_manufacture_cost;
+		static Double Product_selling_price;
+		static String Product_distrobution_point;
+		static Integer Product_reorder_amount;
+		static Double Product_Storing_temprature;
+		
+		
+	public static String Return_Capital()
+	{
+		return null;
+		
+	}
+	
+	public static String View_Inventory()
+	{
+		return null;
+	}
+	
+	public static Store_Object()
+	{
+		return null;
+	}
+	
+	private void new_store(String name, double capital)
+	{
+		Store.name = name;//the stores name like jack and jills inc
+		Store.capital = capital;//initiall funds in the store
+		Store.inventory = null;//a new store shouldnt have anything 
+		
+	}
+	
+	
+	
+}
