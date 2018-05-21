68090838b3c855fec367d142e5bd7f0c33543155
Jared Carey <itspandorux@gmail.com>
2018-05-21 22:40:39 +1000

    Placeholder CSV Parsing Methods Added
    - CSV Parsing Method added to Manifest
    - CSV Parsing Method added to Stock

--- src/delivery/Manifest.java
+++ src/delivery/Manifest.java
@@ -1,5 +1,39 @@
 package delivery;
 
-public class Manifest {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import exceptions.CSVFormatException;
+import store.CSVException;
+import store.Item;
+
+public class Manifest {
+	
+	public static ArrayList<Item> importItemCSV(String filePath) throws CSVException{
+		
+		File file = new File(filePath);
+		Scanner inputStream = new Scanner(file);
+		inputStream.useDelimiter("(\\s\n)"); // Separates CSV lines
+		
+
+		try {	
+			ArrayList<Item> items = new ArrayList<Item>();
+			while (inputStream.hasNext()) {
+				String[] params = inputStream.next().split(",");
+				
+					// TODO: Append to Manifest
+						
+					default:
+						throw CSVFormatException();
+						break;
+				}
+			}
+			return items;
+		}
+		catch (CSVFormatException e) {
+			
+		}	
+	}	
+	
 }
--- src/store/Stock.java
+++ src/store/Stock.java
@@ -1,8 +1,32 @@
 package store.Reagan_assesment_supermart;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import exceptions.CSVFormatException;
 
 public class Stock {
 
+	
+	public static ArrayList<Item> importItemCSV(String filePath) throws CSVException{
+		
+		File file = new File(filePath);
+		Scanner inputStream = new Scanner(file);
+		inputStream.useDelimiter("(\\s\n)"); // Separates CSV lines
+		
 
+		try {	
+			ArrayList<Item> items = new ArrayList<Item>();
+			while (inputStream.hasNext()) {
+				String[] params = inputStream.next().split(",");
+				// TODO: Append to Sales Log
+			}
+			return items;
+		}
+		catch (CSVFormatException e) {
+			
+		}	
+	}	
+	
 }
