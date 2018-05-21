c778c77b7adfabe9824ef550747601b6c4a745ad
Jared Carey <itspandorux@gmail.com>
2018-05-21 23:02:03 +1000

    Partially Implemented Manifest CSV Parser

--- src/delivery/Manifest.java
+++ src/delivery/Manifest.java
@@ -10,23 +10,41 @@ import store.Item;
 
 public class Manifest {
 	
+	
+	/**
+	 * @author Jared Carey
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws CSVException
+	 */
 	public static ArrayList<Item> importItemCSV(String filePath) throws CSVException{
 		
 		File file = new File(filePath);
 		Scanner inputStream = new Scanner(file);
-		inputStream.useDelimiter("(\\s\n)"); // Separates CSV lines
-		
+		inputStream.useDelimiter(">"); // Separates CSV lines by Truck Groupings
+		TruckFactory truckCreator = new TruckFactory();
 
 		try {	
-			ArrayList<Item> items = new ArrayList<Item>();
+			ArrayList<TruckBase> items = new ArrayList<TruckBase>();
 			while (inputStream.hasNext()) {
 				String[] params = inputStream.next().split(",");
 				
-					// TODO: Append to Manifest
+				switch(params[0]) {
+					
+					case "Ordinary":
+						truckCreator.createTruck("Ordinary");
+						break;
+						
+					case "Refrigerated":
+						truckCreator.createTruck("Refrigerated");
+						break;
 						
 					default:
 						throw CSVFormatException();
-						break;
+				}
+					// TODO: Append to Manifest
+						
 				}
 			}
 			return items;
